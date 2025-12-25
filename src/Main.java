d0d554c9faee920d3b954fd2a28b4e1f494cdbf1
imam <ihossai1@stu.vistula.edu.pl>
2025-12-25 22:33:30 +0100

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+enum TrafficLight {
+    RED,
+    YELLOW,
+    GREEN
+}
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+
+class TrafficLightSimulator {
+
+    private TrafficLight currentLight;
+
+    public TrafficLightSimulator(TrafficLight initialLight) {
+        this.currentLight = initialLight;
+    }
+
+    public void changeColor(TrafficLight light) {
+        switch (light) {
+            case RED:
+                currentLight = TrafficLight.GREEN;
+                break;
+            case GREEN:
+                currentLight = TrafficLight.YELLOW;
+                break;
+            case YELLOW:
+                currentLight = TrafficLight.RED;
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Current traffic light: " + currentLight;
+    }
+
+    public TrafficLight getCurrentLight() {
+        return currentLight;
+    }
+}
+
+// Main class
+public class Main {
+    public static void main(String[] args) {
+
+        TrafficLightSimulator simulator =
+                new TrafficLightSimulator(TrafficLight.RED);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(simulator.toString());
+            simulator.changeColor(simulator.getCurrentLight());
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
